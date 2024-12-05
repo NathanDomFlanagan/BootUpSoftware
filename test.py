d053b8c78a9d49ee4cfb10d75e7e547d11c4582d
Nathan Flanagan <jaypedesh7@gmail.com>
2024-12-05 16:53:04 +1300

    Update test.py

--- test.py
+++ test.py
@@ -16,23 +16,29 @@ def main():
         print("Running application at startup...")
 
     categories = {
+        "default": [
+            r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe", "pycharm"],
         "gaming": [
             r"C:\Program Files (x86)\Steam\Steam.exe", "epicgames"],  # Placeholders for now
-        "programming": ["code", "pycharm"]  # Placeholders for now
+        "programming": [
+            "code", "pycharm"]  # Placeholders for now
     }
 
-    print("What do you plan to do today?")
-    for category in categories:
-        print(f"- {category.capitalize()}")
+    default_category = "default" # Set default category 
+    # Prompt for category 
+    print("What do you plan to do today?") 
+    for category in categories: 
+        print(f"- {category.capitalize()}") 
+        
+    chosen_category = input("Choose a category: ").strip().lower() 
     
-    chosen_category = input("Choose a category: ").strip().lower()
+    if chosen_category not in categories: 
+        print(f"No valid category chosen, defaulting to {default_category.capitalize()}.") 
+        chosen_category = default_category 
     
-    if chosen_category in categories:
-        print(f"Starting applications for {chosen_category}...")
-        for app in categories[chosen_category]:
-            start_app([app])
-    else:
-        print("Invalid category. Please choose 'gaming' or 'programming'.")
+    print(f"Starting applications for {chosen_category}...") 
+    for app in categories[chosen_category]: 
+        start_app([app])
 
 if __name__ == "__main__":
     main()
